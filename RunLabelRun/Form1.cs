bcce3a74fb7c97fba61c2efccad73b3c795a8ee3
srvr-2 <srvr-2@mtk.local>
2018-01-30 11:41:38 +0300

    Initial commit.

---
+++ RunLabelRun/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RunLabelRun
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Runer[] runer = new Runer[3];
+
+        private void ToggleRun(int index, Label labelRuner)
+        {
+            if (runer[index]==null)
+            {
+                runer[index] = new Runer();
+                runer[index].MyLabel = labelRuner;
+            }
+            else
+            {
+                runer[index] = null;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ToggleRun(0, label1);
+            timer1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ToggleRun(1, label2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ToggleRun(2, label3);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (runer[i]!=null)
+                {
+                    runer[i].Move();
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Stop();
+            }
+            else
+            {
+                timer1.Start();
+            }
+        }
+    }
+}
---
+++ RunLabelRun/Runer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RunLabelRun
+{
+    class Runer
+    {
+        public Label MyLabel;
+        public bool GoingToForward = true;
+
+        public void Move()
+        {
+            if (MyLabel!=null)
+            {
+                if (GoingToForward)
+                {
+                    MyLabel.Left += 5;
+                    if (MyLabel.Left>=MyLabel.Parent.Width-MyLabel.Width)
+                    {
+                        GoingToForward = false;
+                    }
+                }
+                else
+                {
+                    MyLabel.Left -= 5;
+                    if (MyLabel.Left<=0)
+                    {
+                        GoingToForward = true;
+                    }
+                }
+            }
+        }
+    }
+}
